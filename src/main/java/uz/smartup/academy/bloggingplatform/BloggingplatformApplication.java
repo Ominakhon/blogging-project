977c63fa319ab5fd0b5f36be77b9d61252f9aac0
AbdullakhMirfayziev <abdullakhmirfayziev81@gmail.com>
2024-06-20 13:08:47 +0500

    Project started

---
+++ src/main/java/uz/smartup/academy/bloggingplatform/BloggingplatformApplication.java
@@ -0,0 +1,13 @@
+package uz.smartup.academy.bloggingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/smartup/academy/bloggingplatform/BloggingplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.smartup.academy.bloggingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
