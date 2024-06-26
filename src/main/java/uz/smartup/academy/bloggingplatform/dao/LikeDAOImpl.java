1e166b016cabe37ecea81ebc07306c76630f979a
Asadbek Axmedov <100250308+asadbekakmedov2001@users.noreply.github.com>
2024-06-26 10:14:00 +0500

    Like dao, dto, service

---
+++ src/main/java/uz/smartup/academy/bloggingplatform/dao/LikeDAOImpl.java
@@ -0,0 +1,37 @@
+package uz.smartup.academy.bloggingplatform.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import uz.smartup.academy.bloggingplatform.entity.Like;
+
+public class LikeDAOImpl implements LikeDAO {
+
+    private final EntityManager entityManager;
+
+    public LikeDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Transactional
+    @Override
+    public Like findByUserAndPost(int userId, int postId) {
+        Like like = entityManager.createQuery(
+                "SELECT l FROM Like l WHERE l.userId = :userId AND l.postId = :userId", Like.class)
+                .setParameter("userId", userId)
+                .setParameter("userId", postId)
+                .getSingleResult();
+        return like;
+    }
+
+    @Override
+    @Transactional
+    public void save(Like like) {
+        entityManager.persist(like);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Like like) {
+        entityManager.remove(like);
+    }
+}
