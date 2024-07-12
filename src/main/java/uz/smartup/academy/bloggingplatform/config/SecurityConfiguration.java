64c09fae5a52cf26eceff40049dece1c69876d29
Asadbek Axmedov <96821454+asadbek200@users.noreply.github.com>
2024-07-12 11:29:45 +0500

    1. Roli admin bo'lsa login vaqtida admin panelga kirish 2. Userni update qilish 3. Userni delete qilish 4. Categoryni update qilish 5. Categoryni delete qilish

--- src/main/java/uz/smartup/academy/bloggingplatform/config/SecurityConfiguration.java
+++ src/main/java/uz/smartup/academy/bloggingplatform/config/SecurityConfiguration.java
@@ -44,6 +44,7 @@ public class SecurityConfiguration {
         http.authorizeHttpRequests(
                         authManager ->  authManager
                                 .requestMatchers(HttpMethod.GET, "/admin", "/admin/*").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.POST, "/admin", "/admin/*").hasAnyRole("ADMIN")
                                 .requestMatchers(HttpMethod.GET, "/", "/register", "/posts/*", "/profile/*", "/categories/*").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/profile/*", "/register").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/css/**", "/js/**", "/photos/**").permitAll()
--- src/main/java/uz/smartup/academy/bloggingplatform/mvc/AdminPanelController.java
+++ src/main/java/uz/smartup/academy/bloggingplatform/mvc/AdminPanelController.java
@@ -1,12 +1,14 @@
 package uz.smartup.academy.bloggingplatform.mvc;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import uz.smartup.academy.bloggingplatform.dto.*;
-import uz.smartup.academy.bloggingplatform.entity.Post;
 import uz.smartup.academy.bloggingplatform.service.*;
 
 import java.util.List;
@@ -29,10 +31,14 @@ public class AdminPanelController {
         this.commentService = commentService;
         this.likeService = likeService;
         this.tagService = tagService;
+
     }
 
-    @RequestMapping("/admin")
+    @GetMapping("/admin")
     public String admin(Model model) {
+        UserDTO user =  userService.getUserByUsername(getLoggedUser().getUsername());
+        model.addAttribute("user",user);
+
         List<UserDTO> userDTOS = userService.getAllUsers();
         List<PostDto> postDtos = postService.getAllPosts();
         List<CategoryDto> categories = categoryService.getAllCategories();
@@ -44,9 +50,20 @@ public class AdminPanelController {
 
         return "admin_zip/admin_panel";
     }
+    private UserDetails getLoggedUser() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails)
+            return (UserDetails) principal;
+
+        return null;
+    }
 
     @GetMapping("/admin/category/{title}")
     public String getPostsByCategory(@PathVariable("title") String title, Model model) {
+
+        UserDTO user =  userService.getUserByUsername(getLoggedUser().getUsername());
+        model.addAttribute("user",user);
 
         List<CategoryDto> categories = categoryService.getAllCategories();
         List<PostDto> posts = postService.getPostsByCategory(title);
@@ -57,8 +74,12 @@ public class AdminPanelController {
         return "admin_zip/category_table";
     }
 
-    @RequestMapping("/admin/user")
+    @GetMapping("/admin/user")
     public String getAllUsers(Model model) {
+
+        UserDTO user =  userService.getUserByUsername(getLoggedUser().getUsername());
+        model.addAttribute("user",user);
+
         List<UserDTO> userDTOS = userService.getAllUsers();
 
         List<CategoryDto> categories = categoryService.getAllCategories();
@@ -71,8 +92,19 @@ public class AdminPanelController {
         return "admin_zip/user_table";
     }
 
-    @RequestMapping("/admin/post")
+    @GetMapping("/admin/user/delete/{id}")
+    public String deleteUser(@PathVariable int id){
+        userService.deleteUser(id);
+        return "redirect:/admin/user";
+    }
+
+    @GetMapping("/admin/post")
     public String getAllPost(Model model) {
+
+        UserDTO user =  userService.getUserByUsername(getLoggedUser().getUsername());
+        model.addAttribute("user",user);
+
+
         List<PostDto> postDtos = postService.getAllPosts();
 
         List<CategoryDto> categories = categoryService.getAllCategories();
@@ -85,8 +117,13 @@ public class AdminPanelController {
         return "admin_zip/post_table";
     }
 
-    @RequestMapping("/admin/comment")
+    @GetMapping("/admin/comment")
     public String getAllComment(Model model) {
+
+        UserDTO user =  userService.getUserByUsername(getLoggedUser().getUsername());
+        model.addAttribute("user",user);
+
+
         List<CommentDTO> commentDTOS = commentService.getAllComments();
         List<CategoryDto> categories = categoryService.getAllCategories();
 
@@ -98,8 +135,13 @@ public class AdminPanelController {
         return "admin_zip/comment_table";
     }
 
-    @RequestMapping("/admin/like")
+    @GetMapping("/admin/like")
     public String getAllLike(Model model) {
+
+        UserDTO user =  userService.getUserByUsername(getLoggedUser().getUsername());
+        model.addAttribute("user",user);
+
+
         List<LikeDTO> likeDTOS = likeService.getAllLikes();
         List<CategoryDto> categories = categoryService.getAllCategories();
 
@@ -111,20 +153,13 @@ public class AdminPanelController {
         return "admin_zip/like_table";
     }
 
-    @RequestMapping("/admin/category")
-    public String getAllCategories(Model model) {
-        List<CategoryDto> categories = categoryService.getAllCategories();
-
-        model.addAttribute("categoryDTO",categories);
-        model.addAttribute("categorySize",categories.size());
-
-        model.addAttribute("categories",categories);
-
-        return "admin_zip/categories_table";
-    }
-
-    @RequestMapping("/admin/tag")
+    @GetMapping("/admin/tag")
     public String getAllTag(Model model) {
+
+        UserDTO user =  userService.getUserByUsername(getLoggedUser().getUsername());
+        model.addAttribute("user",user);
+
+
         List<TagDto> tagDtos = tagService.getAllTags();
         List<CategoryDto> categories = categoryService.getAllCategories();
 
@@ -136,8 +171,47 @@ public class AdminPanelController {
         return "admin_zip/tag_table";
     }
 
+    @GetMapping("/admin/category")
+    public String getAllCategories(Model model) {
+
+        UserDTO user =  userService.getUserByUsername(getLoggedUser().getUsername());
+        model.addAttribute("user",user);
 
 
+        List<CategoryDto> categories = categoryService.getAllCategories();
 
+        model.addAttribute("categoryDTO",categories);
+        model.addAttribute("categorySize",categories.size());
 
+        model.addAttribute("categories",categories);
+
+        return "admin_zip/categories_table";
+    }
+
+    @PostMapping("/admin/category/add")
+    public String addCategory(CategoryDto categoryDto){
+        categoryService.createCategory(categoryDto);
+        return "redirect:/admin/category";
+    }
+
+    int category_id;
+    @GetMapping("/admin/category/edit/{id}")
+    public String editCategory(@PathVariable int id, Model model){
+        category_id = id;
+        model.addAttribute("categoryDTO",categoryService.findCategoryById(id));
+        return "admin_zip/categories_edit.html";
+    }
+
+    @PostMapping("/admin/category/edit")
+    public String editCategory(@ModelAttribute CategoryDto categoryDTO){
+        categoryDTO.setId(category_id);
+        categoryService.update(categoryDTO);
+        return "redirect:/admin/category";
+    }
+
+    @GetMapping("/admin/category/delete/{id}")
+    public String deleteCategory(@PathVariable int id){
+        categoryService.delete(id);
+        return "redirect:/admin/category";
+    }
 }
